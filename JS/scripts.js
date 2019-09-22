ee003e71ea2fa80c2856bdd35d8632b7101066d8
Noah Horwitz <00noah@gmail.com>
2019-09-22 16:13:23 -0700

    start entering JS lines

---
+++ JS/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    let nameInput = ($("#name").val());
+    // let side2Input = parseInt($("#side2").val());
+    // let side3Input = parseInt($("#side3").val());
+
+
+    $(".jumbotron").show();
+    if (side1Input === side2Input && side2Input === side3Input) {
+      $("#results").text("an Equilateral");
+    }
+    else if (side1Input === side2Input || side2Input === side3Input || side1Input === side3Input) {
+      $("#results").text("an Isoceles");
+    }
+    else if (side1Input + side2Input <= side3Input || side2Input + side3Input <= side1Input || side1Input + side3Input <= side2Input) {
+      $("#results").text("something other than a");
+    }
+    else {
+      $("#results").text("a Scalene");
+    }
+
+    let elmnt = document.getElementById("results");
+    elmnt.scrollIntoView();
+
+  });
+});
