b52574e3e564b12937e963172cf5986a956391a1
Noah Horwitz <00noah@gmail.com>
2019-09-22 21:39:54 -0700

    complete show-hide branching in JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
   $("form#questions").submit(function(event) {
     event.preventDefault();
 
-		$(".jumbotron").show();
-
 // VARIABLES:
 
 		var nameInput = $("#firstName").val();
@@ -26,33 +24,50 @@ $(document).ready(function() {
 
 // BRANCHING:
 
+    $(".jumbotron").show();
+
     var math = (hoursInput + adventurousInput + frontOrBackInput + popularityInput + languageInput);
     $(".math").text(math);
 
     if (math <= 2) {
       $(".results").text("RPG III");
       $(".RPG").show();
+      $(".Python, .Ruby, .JavaScript, .C, .Go, .Swift, .V").hide();
     }
     else if (math <= 7) {
       $(".results").text("V");
+      $(".V").show();
+      $(".RPG, .Python, .Ruby, .JavaScript, .C, .Go, .Swift").hide();
     }
     else if (math <= 9) {
       $(".results").text("Ruby");
+      $(".Ruby").show();
+      $(".RPG, .Python, .JavaScript, .C, .Go, .Swift, .V").hide();
     }
     else if (math <= 11) {
       $(".results").text("C#");
+      $(".C").show();
+      $(".RPG, .Python, .Ruby, .JavaScript, .Go, .Swift, .V").hide();
     }
     else if (math <= 14) {
       $(".results").text("Go");
+      $(".Go").show();
+      $(".RPG, .Python, .Ruby, .JavaScript, .C, .Go, .Swift, .V").hide();
     }
     else if (math <= 17) {
       $(".results").text("Swift");
+      $(".Swift").show();
+      $(".RPG, .Python, .Ruby, .JavaScript, .C, .Go, .V").hide();
     }
     else if (math <= 21) {
       $(".results").text("Python");
+      $(".Python").show();
+      $(".RPG, .Ruby, .JavaScript, .C, .Go, .Swift, .V").hide();
     }
     else {
       $(".results").text("JavaScript");
+      $(".JavaScript").show();
+      $(".RPG, .Python, .Ruby, .C, .Go, .Swift, .V").hide();
     }
 
 
